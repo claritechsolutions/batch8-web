b575877f98b0c155d37976b0617cf03b0dd76308
Akash-Edake <akashedake30@gmail.com>
2022-04-18 08:24:50 +0530

    code-push

---
+++ day-3/day-3.js
@@ -0,0 +1,11 @@
+var data = 0;
+document.getElementById("output").innerHTML = data;
+
+function addition() {
+    data = data + 3;
+    document.getElementById("output").innerHTML = data;
+}
+function subtraction() {
+    data = data - 3;
+    document.getElementById("output").innerHTML = data;
+}
