1bb1e08e6bb37be7d047e6cf981d1adf1a35c81a
handepriyanka <priyankahande92@gmail.com>
2022-04-19 00:01:17 -0700

    change done

---
+++ Day3/day3.js
@@ -0,0 +1,18 @@
+var data = 0;
+
+ document.getElementById("output").innerText = data;
+ //document.getElementById("output").innerHtml = data;
+
+function pluseOne(){
+    data = data+1;
+    var inText =document.getElementById("output").innerText = data;
+    //console.log(inText);
+
+}
+
+function minusOne(){
+    data = data-1;
+    var inText =document.getElementById("output").innerText = data;
+   //console.log(inHtml);
+}
+
