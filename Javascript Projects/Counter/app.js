d11eb1c4dc46a8b940d650e3824056763ca30068
bhupen2412 <batra.bhupen@gmail.com>
2022-04-22 02:32:34 +0530

    code updated

--- Javascript Projects/Counter/app.js
+++ Javascript Projects/Counter/app.js
@@ -1,22 +1,27 @@
 let value = document.querySelector('.display')
 let buttons = document.querySelectorAll('.btn')
 let count = 0;
-buttons.forEach(function(key){
-    key.addEventListener('click', function(e){
-       let btn = e.currentTarget.innerText;
-       if(btn == 'Decrease')
-       {
-           count --;
-       }
-       else if(btn == 'Increase')
-       {
-           count ++;
-       }
-       else if(btn == 'Reset')
-       {
-           count = 0;
-       }
-       value.textContent = count;
+buttons.forEach(function (key) {
+    key.addEventListener('click', function (e) {
+        let btn = e.currentTarget.innerText;
+        if (btn == 'Decrease') {
+            count--;
+        }
+        else if (btn == 'Increase') {
+            count++;
+        }
+        else if (btn == 'Reset') {
+            count = 0;
+        }
+        if (count > 0)
+            value.setAttribute("style", "color:green;");
+        else if (count < 0)
+            value.setAttribute("style", "color:red;");
+        else
+            value.setAttribute("style", "color:black;");
+
+        value.textContent = count;
+
 
     })
 })
