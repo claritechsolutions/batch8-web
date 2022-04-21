b2618b580634ca0c37c60bcb765782dc775fea30
bhupen2412 <batra.bhupen@gmail.com>
2022-04-22 01:19:18 +0530

    Code updated

--- Javascript Projects/Calculator/app.js
+++ Javascript Projects/Calculator/app.js
@@ -1,15 +1,37 @@
-let screen = document.getElementById('screen');
+let screen = document.getElementById('currentValue');
+let prev = document.getElementById('previousValue');
 buttons = document.querySelectorAll('button');
 let calc = '';
+prev.value=0;
+screen.value=0;
+
 for (item of buttons) {
     item.addEventListener('click', (e) => {
         buttonText = e.target.innerText;
-        if (buttonText == 'C') {
-            calc = "";
+        if(buttonText == '÷'){
+            buttonText='/';
+            calc += buttonText;
             screen.value = calc;
         }
+        else if(buttonText == 'x'){
+            buttonText='*';
+            calc += buttonText;
+            screen.value = calc;
+        }
+        else if (buttonText == 'C') {
+            calc = "";
+            screen.value = calc;
+            
+        }
         else if (buttonText == '=') {
-            screen.value = eval(calc);
+            calc = eval(calc);
+            screen.value = calc;
+            prev.value = calc;
+        }
+        else if (buttonText == 'Ans') {
+            calc += prev.value;
+            screen.value += 'Ans';
+            
         }
         else {
             calc += buttonText;
@@ -18,33 +40,5 @@ for (item of buttons) {
 
     })
 }
-
-
-
-// // (function () {
-//     let screen = document.querySelector('.calculator__display');
-//     let buttons = document.querySelectorAll('.btn');
-//     let equals = document.querySelector('.key-equal');
-//     let clear = document.querySelector('.clearAll');
-//     let screenValue="";
-//         for (item of buttons) {
-//         item.addEventListener('click', (e) => {
-//             buttonText = e.target.innerText;
-//             console.log('Button text is ', buttonText);
-            
-//             if (buttonText == 'C') {
-//                 screenValue = "";
-//                 screen.value = screenValue;
-//             }
-//             else if (buttonText == '=') {
-//                 screen.value = eval(screenValue);
-//             }
-//             else {
-//                 screenValue += buttonText;
-//                 screen.value = screenValue;
-//             }
-//         })
-//     }
-// // })
 
 
