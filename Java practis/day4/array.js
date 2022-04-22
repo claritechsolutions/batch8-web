546ffae3eb9d23ca9de4460839a876e4faa2d65e
prajakta <prajaktamekhale@gmail.com>
2022-04-22 22:50:44 +0530

    send to

---
+++ Java practis/day4/array.js
@@ -0,0 +1,17 @@
+
+var bikes = ["yamaha","pulsar","apache","raider","jupiter"];
+
+
+var cars = ["venue","swift","thar","bugatti","creta",];
+
+document.write( "List of cars" +"<ul>");{
+    for(var a = 0; a <= 4; a++)
+    document.write("<li>" + cars[a] + "<br>" +"</li>");
+}
+document.write("</ul>");
+
+document.write( "List of bikes" +"<ul>");{
+    for(var a = 0; a <= 4; a++)
+    document.write("<li>" + bikes[a] + "<br>" +"</li>");
+}
+document.write("</ul>");
