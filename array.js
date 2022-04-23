b67afc51490f3cf2dd28c2d62035460184a12f2c
handepriyanka <priyankahande92@gmail.com>
2022-04-23 13:04:49 +0530

    array

---
+++ array.js
@@ -0,0 +1,37 @@
+
+let allUser = [
+
+    {
+        name: "xyz",
+        age: "21",
+        addr: "lan1",
+    },
+    {
+        name: "abc",
+        age: "21",
+        addr: "lan2",
+
+    },
+    {
+        name: "abc",
+        age: "21",
+        addr: "lan2",
+    },
+    {
+        items:
+        {
+            name: "fg",
+            age: "22",
+            addr: "lan4",
+        }
+    
+
+    }
+
+
+]
+console.log("user details=>")
+console.log(allUser);
+console.log("items=>")
+console.log(allUser.items);
+//console.log("allUser").addr[0];
