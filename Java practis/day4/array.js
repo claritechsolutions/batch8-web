3b1d6169b62f4a0494f737d3528670937d8d45f4
prajakta <prajaktamekhale@gmail.com>
2022-04-24 13:34:15 +0530

    send

--- Java practis/day4/array.js
+++ Java practis/day4/array.js
@@ -12,6 +12,6 @@ document.write("</ul>");
 
 document.write( "List of bikes" +"<ul>");{
     for(var a = 0; a <= 4; a++)
-    document.write("<li>" + bikes[a] + "<br>" +"</li>");
+    document.write("<li>" + cars[a] + "<br>" +"</li>");
 }
 document.write("</ul>");
