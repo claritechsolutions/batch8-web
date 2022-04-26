5ebcd340b016457bbd587036b30a8b6209d4ec6f
aaleshpatil22@gmail.com <aaleshpatil22@gmail.com>
2022-04-26 06:59:21 +0530

    new-javascrpt

---
+++ JAVASCRIPT/day-6/day-6.js
@@ -0,0 +1,35 @@
+let alphabet=["a","b","c","d","e","f","g","h","i","j","k"];
+
+document.write("Array--------> ",alphabet,"</br>");
+
+document.write("Type---------> ",typeof(alphabet),"</br>");
+
+document.write("Tostring--------> ",alphabet.toString(),"</br>");
+
+document.write("Spacing--------> ",alphabet.join("+++"),"</br>");
+
+document.write("length---------->",alphabet.length,"</br>");
+
+document.write("includes---------->",alphabet.includes("e"),"</br>");
+
+document.write("includes---------->",alphabet.includes("z"),"</br>");
+
+document.write("push l---------->",alphabet.push("l"),"</br>");
+
+document.write("push l--------> ",alphabet,"</br>");
+
+document.write("pop l---------->",alphabet.pop("l"),"</br>");
+
+document.write("index---------->",alphabet.indexOf("l"),"</br>");
+
+document.write("index---------->",alphabet.indexOf("b"),"</br>");
+
+document.write("pop l--------> ",alphabet,"</br>");
+
+document.write("splice---------->",alphabet.splice("-2"),"</br>");
+
+document.write("splice--------> ",alphabet,"</br>");
+
+document.write("splice---------->",alphabet.splice("2"),"</br>");
+
+document.write("splice--------> ",alphabet,"</br>");
